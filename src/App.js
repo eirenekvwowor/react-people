2b5acb61290a91582ce1fe8e42928ece9c6d67e0
eirenekvwowor <eirene.wowor@gmail.com>
2020-11-13 05:49:16 +0700

    Push tugas absent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const sortByPower = (a,b) => 
+{
+  return b.power - a.power;
+}
+const List = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>React People!</h1>
+    <ul className ="list">
+      {props.data.sort(sortByPower).map((person) => (
+        <li key= {person.name} className="list-item">
+          <div className="list-item-image-container">
+            <img src={person.thumbnail}/>
+          </div>
+          <span className="list-item-name">
+          {person.name} (Power:{' '}
+        <span className="list-item-power">{person.power}</span> )
+          </span>
+        </li>
+      ))}
+    </ul>
+    </>
   );
 }
-
+const App = () => {
+  const human = [
+    {
+      name: 'Kurikin',
+      thumbnail:
+      'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
+      power: 10
+    },
+    {
+      name: 'Bulma',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
+      power: 3
+    },
+    {
+      name: 'Chi-chi',
+      thumbnail:
+      'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
+      power: 30
+    }
+  ];
+return (
+  <div className="App">
+    <List data={human}/>
+  </div>
+);
+}
 export default App;
